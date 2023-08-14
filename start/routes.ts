95cbac5fe774752633b18ef9cd99532b3fe59d5d
khoirulh1610 <rizalgaras@gmail.com>
2023-08-14 16:59:57 +0700

    use navigate

--- start/routes.ts
+++ start/routes.ts
@@ -21,11 +21,7 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-
-
-Route.group(() => {
-  Route.resource('/note', 'NotesController')
-}).middleware('auth')
+Route.resource('/note', 'NotesController').middleware('auth')
 
 
 Route.post('/register', 'AuthController.register')
